ca402317418f89023979a908bf058a44a8cc86b7
Zyrel <zyrel05pasion1105@gmail.com>
2019-12-18 13:07:22 +0800

    12-18-2019 testFragment added done

--- Kotlin/app/src/main/java/com/example/blapp/SetStepFragment.kt
+++ Kotlin/app/src/main/java/com/example/blapp/SetStepFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentController
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_program.*
 import kotlinx.android.synthetic.main.fragment_set_step.*
 
 /**
@@ -16,11 +17,20 @@ import kotlinx.android.synthetic.main.fragment_set_step.*
  */
 class SetStepFragment : Fragment() {
 
+    lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_set_step, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        btn_pgm_step.setOnClickListener{
+            navController.navigate(R.id.action_setStepFragment_to_programFragment)
+        }
     }
 }

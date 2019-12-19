a03dee55bae1b79388023cfe566116dbe2a63985
markwiliz30 <markwiliz30@yahoo.com>
2019-12-19 16:57:08 +0800

    channel oke

--- Kotlin/app/src/main/java/com/example/blapp/TestFragment.kt
+++ Kotlin/app/src/main/java/com/example/blapp/TestFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_test.*
@@ -26,8 +27,48 @@ class TestFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        tglPgm1.setOnClickListener{
+            Toast.makeText(context, "Testing Program 1 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm2.setOnClickListener{
+            Toast.makeText(context, "Testing Program 2 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm3.setOnClickListener{
+            Toast.makeText(context, "Testing Program 3 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm4.setOnClickListener{
+            Toast.makeText(context, "Testing Program 4 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm5.setOnClickListener{
+            Toast.makeText(context, "Testing Program 5 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm6.setOnClickListener{
+            Toast.makeText(context, "Testing Program 6 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm7.setOnClickListener{
+            Toast.makeText(context, "Testing Program 7 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm8.setOnClickListener{
+            Toast.makeText(context, "Testing Program 8 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm9.setOnClickListener{
+            Toast.makeText(context, "Testing Program 9 Running", Toast.LENGTH_SHORT).show()
+        }
+
+        tglPgm10.setOnClickListener{
+            Toast.makeText(context, "Testing Program 10 Running", Toast.LENGTH_SHORT).show()
+        }
+
         btnReset.setOnClickListener{
-            //navController.navigate(R.id.action_testFragment_to_landingFragment)
+            Toast.makeText(context, "Resetting the device", Toast.LENGTH_SHORT).show()
         }
     }
 }
---
+++ Kotlin/app/src/main/java/com/example/blapp/programitem/PgmItem.kt
@@ -0,0 +1,13 @@
+package com.example.blapp.programitem
+
+class PgmItem {
+    var name: String? = null
+    var pgm: Byte? = 0
+
+    //Getter
+    get() = field
+    //Setter
+    set(value) {
+        field = value
+    }
+}

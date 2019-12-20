59a437e355591f6741ba83ddeedad07c2621c2df
Zyrel <zyrel05pasion1105@gmail.com>
2019-12-20 08:36:48 +0800

    Schedule to sequence

--- Kotlin/app/src/main/java/com/example/blapp/ScheduleFragment.kt
+++ Kotlin/app/src/main/java/com/example/blapp/ScheduleFragment.kt
@@ -6,8 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_schedule.*
 
 class ScheduleFragment : Fragment() {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +22,13 @@ class ScheduleFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_schedule, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        btnManageSequence.setOnClickListener{
+            navController.navigate(R.id.action_scheduleFragment_to_sequence)
+        }
+    }
 
 }

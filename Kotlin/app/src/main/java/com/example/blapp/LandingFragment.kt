fc470da4278b9dffc0f5a3861124f106537025a2
Zyrel <zyrel05pasion1105@gmail.com>
2020-01-28 16:59:23 +0800

    connect method created

--- Kotlin/app/src/main/java/com/example/blapp/LandingFragment.kt
+++ Kotlin/app/src/main/java/com/example/blapp/LandingFragment.kt
@@ -24,10 +24,13 @@ import com.example.blapp.common.SharedWifiDetails
 import com.example.blapp.model.WifiItem
 import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.fragment_landing.*
+import android.net.wifi.WifiConfiguration
+
 
 /**
  * A simple [Fragment] subclass.
  */
+@Suppress("DEPRECATION")
 class LandingFragment : Fragment() {
     lateinit var navController: NavController
     private val deviceProtocol = DeviceProtocol()
@@ -149,6 +152,85 @@ class LandingFragment : Fragment() {
 //            }
 //            postDelayedSendToModule.postDelayed(sendToModule, 10000)
         }
+    }
+
+    fun connectWiFi(SSID: String, password: String, Security: String) {
+        try {
+
+            val conf = WifiConfiguration()
+            conf.SSID =
+                "\"" + SSID + "\""   // Please note the quotes. String should contain ssid in quotes
+            conf.status = WifiConfiguration.Status.ENABLED
+            conf.priority = 40
+            // Check if security type is WEP
+            if (Security.toUpperCase().contains("WEP")) {
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE)
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN)
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA)
+                conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN)
+                conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED)
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP)
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104)
+
+                if (password.matches("^[0-9a-fA-F]+$".toRegex())) {
+                    conf.wepKeys[0] = password
+                } else {
+                    conf.wepKeys[0] = "\"" + password + "\""
+                }
+
+                conf.wepTxKeyIndex = 0
+                // Check if security type is WPA
+            } else if (Security.toUpperCase().contains("WPA")) {
+
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN)
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA)
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK)
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP)
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP)
+
+                conf.preSharedKey = "\"" + password + "\""
+                // Check if network is open network
+            } else {
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE)
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN)
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA)
+                conf.allowedAuthAlgorithms.clear()
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP)
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP)
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP)
+            }
+            //Connect to the network
+            val wifiManager =
+                activity!!.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            val networkId = wifiManager.addNetwork(conf)
+
+            val list = wifiManager.configuredNetworks
+            for (i in list) {
+                if (i.SSID != null && i.SSID == "\"" + SSID + "\"") {
+
+                    val isDisconnected = wifiManager.disconnect()
+
+                    val isEnabled = wifiManager.enableNetwork(i.networkId, true)
+
+                    val isReconnected = wifiManager.reconnect()
+
+                    break
+                }
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 
 //    private fun generateItem() {
